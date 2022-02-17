6770280c41aeaa5268919c12c087c27993303d36
pedro <pedrolelis@hotmail.com>
2022-02-17 19:42:12 -0300

    constantes e tipos de saída

---
+++ aula_06/aula_06.cs
@@ -0,0 +1,10 @@
+using System;
+
+class aula_06{
+    static void Main(){
+        //Constantes e tipo de saída utilizando {}
+        const string nome = "Pedro";
+        const double pi = 3.1415;
+        Console.WriteLine("Canal: {0}, PI: {1}", nome,pi);
+    }
+}
