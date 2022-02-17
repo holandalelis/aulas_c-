a004c840657049fd5ea2c024a7576fe1b4cb5070
pedro <pedrolelis@hotmail.com>
2022-02-17 18:57:30 -0300

    aula variaveis e escopo

---
+++ Aula_02/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Aula_02
+{
+    class program{
+        static void Main (string[] args){
+            Console.WriteLine("Hello, World!");
+        }
+}
+}
---
+++ aula_03/aula03.cs
@@ -0,0 +1,14 @@
+using System;
+
+class aula_03{
+    static void Main(){
+         //Tipos de Variáveis
+        int num;
+        char letra;
+        float valor = 5.3f;//tem que usar o f no final!!!
+        var nome = "Pedro";
+        Console.WriteLine(nome);
+
+    }
+     
+}
