78339dd27648183cbba7c380429c7b1c2da1fcc3
pedro <pedrolelis@hotmail.com>
2022-02-19 10:16:09 -0300

    construtores

--- aula_construtores/aulaconstrutor.cs
+++ aula_construtores/aulaconstrutor.cs
@@ -4,16 +4,23 @@ public class Jogador{
     int energia;
     bool vivo;
     string nome;
-    public Jogador(string nome){
-        this.nome=nome;
-        energia=100;
-        vivo=true;
+
+    public Jogador(string nome, bool vivo, int energia){
+        this.nome = nome;
+        this.vivo = vivo;
+        this.energia = energia;
+    }
+    public void info(){
+        Console.WriteLine("Nome do Jogador: {0}", nome);
+        Console.WriteLine("Energia do Jogador: {0}", energia);
+        Console.WriteLine("Jogador está vivo: {0}", vivo);
     }
 }
 
 public class aulaconstrutores{
    public static void Main(){
-       Jogador j1 = new Jogador("Pedro");
-       Console.WriteLine("Dados do Jogador{0}: ", j1);
+       Jogador j1 = new Jogador("Pedro", true, 35);
+       Jogador j2 = new Jogador("Mayara", true, 90);
+       j2.info();
 } 
 }
